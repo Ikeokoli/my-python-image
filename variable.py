5f4b18f21e818b6f004fd24de8a7c2bcd477f210
Ikechukwu Charles Okoli <32238654+Ikeokoli@users.noreply.github.com>
2025-05-18 17:52:58 +0100

    Update variable.py

--- variable.py
+++ variable.py
@@ -2,7 +2,7 @@
 
 # Author: Iyke
 # Date Created: 17th May 2025
-# Last Modified: 17th May 2025
+# Last Modified: 18th May 2025
 # Description: This Script takes in user inputs
 # Usage: Change the file permission to executable and run with (./)
 
@@ -16,10 +16,12 @@ department = input("Enter your department")
 
 staffId = input("ENter your staff ID")
 
+location = input("Enetr your location")
 
 print("Registration is successful")
 
 print("This User is ", firstName, lastName)
 print("Your department is ", department)
 print("Your staffId is ", staffId)
+print("Your location is ", location)
 
